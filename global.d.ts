9203fc13629133261f6dc8874ec57651c4ac754f
boris8055 <boriassiboris2@gmail.com>
2025-11-04 23:15:12 +0400

    fix

---
+++ global.d.ts
@@ -0,0 +1,16 @@
+// global.d.ts
+interface EthereumProvider {
+    request: (args: { method: string; params?: any[] }) => Promise<any>;
+    on?: (event: string, handler: (...args: any[]) => void) => void;
+    removeListener?: (event: string, handler: (...args: any[]) => void) => void;
+    isMetaMask?: boolean;
+  }
+  
+  declare global {
+    interface Window {
+      ethereum?: EthereumProvider;
+      webkitSpeechRecognition?: any;
+    }
+  }
+  
+  export {};
